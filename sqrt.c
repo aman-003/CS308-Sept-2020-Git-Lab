1c8eac2c43ef5ebea220b4b75f53f99b0b841b05
aman-003 <b18003@students.iitmandi.ac.in>
2020-10-14 19:50:50 +0530

    negative allowed

--- sqrt.c
+++ sqrt.c
@@ -15,7 +15,7 @@ int main(int argc, char* argv[]) {
 	int input = atoi(argv[1]);
 	
 	if(input<0){
-		printf("Input Error. Input must be Non-Negative. \n");
+		printf("Sqrt of %d is %f\n",input,sqrt(-1*input));
 		exit(-1);
 	}
 
